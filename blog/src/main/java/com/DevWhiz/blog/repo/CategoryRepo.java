76c6f4f96abcb3c6075cd27d795f1187d1a05173
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2025-06-21 13:37:02 +0530

    config to jpa

---
+++ blog/src/main/java/com/DevWhiz/blog/repo/CategoryRepo.java
@@ -0,0 +1,11 @@
+package com.DevWhiz.blog.repo;
+
+import com.DevWhiz.blog.domain.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepo extends JpaRepository<Category, UUID> {
+}
